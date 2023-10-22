4f3da14cec6e7d7da9b99852068febf70f0e04d3
bennytalks <bennytoeppe@gmail.com>
2023-10-21 23:36:29 -0400

    final changes please work

--- carousel.js
+++ carousel.js
@@ -21,3 +21,65 @@ function showSlide(n) {
     
     slides[slideIndex].style.display = "block";
 }
+function handleButtonClick(btnarrow, content, hiddenText){
+
+let isTextVisible = false;
+
+btnarrow.addEventListener('click', () => {
+  if (isTextVisible) {
+    content.style.transform = 'translateY(0)';
+    hiddenText.style.display = 'none';
+	btnarrow.style.transform = 'scaleY(-1)';
+  } else {
+    content.style.transform = 'translateY(-50%)';
+    hiddenText.style.display = 'block';
+	btnarrow.style.transform = 'scaleY(1)';
+  }
+
+	
+	
+  isTextVisible = !isTextVisible;
+});
+}
+{
+const btnarrows = document.querySelectorAll('.btnarrow');
+btnarrows.forEach(btnarrow => {
+  const container = btnarrow.closest('.grid-item'); // Get the container for this button
+  const content = container.querySelector('.grid-item-text');
+  const hiddenText = container.querySelector('.hidden-text');
+
+  handleButtonClick(btnarrow, content, hiddenText);
+});
+}
+
+function handleButtonClick2(dwnbtnarrow, content, hiddenText) 
+
+{
+  let isTextVisible = false;
+
+  dwnbtnarrow.addEventListener('click', () => {
+    if (isTextVisible) {
+      content.style.transform = 'translateY(0)';
+	hiddenText.style.transform = 'scaleY(0%)';
+      dwnbtnarrow.style.transform = 'scaleY(1)';
+	hiddenText.style.padding = '0px 0px 0px 0px'
+    } else {
+      content.style.transform = 'translateY(50%)';
+      hiddenText.style.display = 'block';
+		hiddenText.style.transform = 'scaleY(100%)';
+		hiddenText.style.padding = '40px 0px 40px 0px';
+      dwnbtnarrow.style.transform = 'scaleY(-1)';
+    }
+
+    isTextVisible = !isTextVisible;
+  });
+}
+
+const dwnbtnarrows = document.querySelectorAll('.downbtnarrow');
+dwnbtnarrows.forEach(dwnbtnarrow => {
+  const container = dwnbtnarrow.closest('.strip'); // Get the container for this button
+  const content = container.querySelector('.dwnbuttoncontainer');
+  const hiddenText = container.querySelector('.dwnbuttoncontainer');
+
+  handleButtonClick2(dwnbtnarrow, content, hiddenText);
+});
